de9c7b71b8d846b0b0ef186a4441e1fa4652806c
zyzh0 <zyzh02@live.com>
2022-07-30 23:32:06 +0800

    optimized

--- AliDNS.py
+++ AliDNS.py
@@ -1,48 +1,34 @@
+
 #!/usr/bin/env python
 # coding=utf-8
 
 from aliyunsdkcore.client import AcsClient
-from aliyunsdkcore.acs_exception.exceptions import ClientException
-from aliyunsdkcore.acs_exception.exceptions import ServerException
 from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
 from aliyunsdkalidns.request.v20150109.UpdateDomainRecordRequest import UpdateDomainRecordRequest
-import json
-import os
 import requests
 
 
 def getIdAndValue(response):
     import json
 
-    DomainRecords = json.loads(str(response, encoding='utf-8'))
-    DomainRecords = DomainRecords['DomainRecords']
-    DomainRecords = DomainRecords['Record']
+    DomainRecords = json.loads(
+        str(response, encoding='utf-8'))['DomainRecords']['Record']
 
-    recLenth = len(DomainRecords) - 1
-
-    i = 0
-    while i <= recLenth:
-        record = DomainRecords[i]
-        if record['RR'] == RR:
-            if record['Type'] == RRtype:
-                recordId = record['RecordId']
-                recordValue = record['Value']
-                break
-        i = i + 1
-    return {'id': recordId, 'value': recordValue}
+    for record in DomainRecords:
+        if record['RR'] == RR and record['Type'] == RRtype:
+            return {'id': record['RecordId'], 'value': record['Value']}
 
 
 def getSystemIP6():
-    text = requests.get('https://v6.ident.me').text
-    return text
+    return requests.get('https://v6.ident.me').text
 
 
-domainName = 'name'
-RR = 'rr'
-RRtype = 'type'
-keyId = 'id'
-keySecret = 'sec'
-location = 'loc'
+domainName = ''
+RR = ''
+RRtype = ''
+keyId = ''
+keySecret = ''
+location = 'cn-hangzhou'
 
 client = AcsClient(keyId, keySecret, location)
 
@@ -50,7 +36,6 @@ request = DescribeDomainRecordsRequest()
 request.set_accept_format('json')
 request.set_DomainName(domainName)
 response = client.do_action_with_exception(request)
-
 
 IdV = getIdAndValue(response)
 sysIP = getSystemIP6()
